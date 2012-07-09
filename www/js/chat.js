4af08ab182d6d51649d688b2e93ce4dfbf1ed9b2
Tim Ermilov <yamalight@gmail.com>
2012-07-09 15:21:40 +0200

    fix chat

--- www/js/chat.js
+++ www/js/chat.js
@@ -36,13 +36,37 @@
     // render page on creation
     $(document).on('pagecreate', "#chatPage", function(){
         chatList = $("#chatList");
+        chatInput = $("#chatInput");
+
+        $("#submitChat").bind(iLepra.config.defaultTapEvent, function(e){
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
+            var text = chatInput.val();
+            chatInput.val("");
+
+            // clear interval to evade overlap
+            window.clearInterval( refreshInterval );
+
+            $.mobile.showPageLoadingMsg();
+            $(document).bind(iLepra.events.ready, function(event){
+                $(document).unbind(event);
+                $.mobile.hidePageLoadingMsg();
+                // get data
+                data = iLepra.chat.messages.slice(0);
+                data.sort(function(a,b){ return a.id > b.id ? -1 : 1});
+                // render
+                refreshMessages();
+                // put refresh interval back
+                refreshInterval = setInterval ( "requestNewChatData()", 10000 );
+            });
+            iLepra.chat.sendMessage(text);
+        });
     });
     $(document).on('pagebeforehide', "#chatPage", function(){
         clearInterval( refreshInterval );
-    	//Ti.App.fireEvent("iLepraChatBar", {show: false});
     });
     $(document).on('pageshow', "#chatPage", function(){
-    	//Ti.App.fireEvent("iLepraChatBar", {show: true});
         window.plugins.nativeUI.setTitle({title: "Лепрочятик", organize: false, refresh: false, menu: true});
 
         $.mobile.showPageLoadingMsg()
@@ -52,34 +76,12 @@
         refreshInterval = window.setInterval(requestNewChatData, 10000 );
     });
 
-    //Ti.App
-    $(document).on("iLepraSubmitChat", function(data){
-        var text = data.val;
-
-        // clear interval to evade overlap
-        window.clearInterval( refreshInterval );
-
-        $.mobile.showPageLoadingMsg();
-        $(document).bind(iLepra.events.ready, function(event){
-            $(document).unbind(event);
-            $.mobile.hidePageLoadingMsg();
-            // get data
-            data = iLepra.chat.messages.slice(0);
-            data.sort(function(a,b){ return a.id > b.id ? -1 : 1});
-            // render
-            refreshMessages();
-            // put refresh interval back
-            refreshInterval = window.setInterval(requestNewChatData, 10000 );
-        });
-        iLepra.chat.sendMessage(text);
-    });
-
     $(document).on("tap", "li.chatMessage", function(e){
         e.preventDefault();
         e.stopImmediatePropagation();
 
         var username = $(this).data('user');
 
-        //Ti.App.fireEvent("iLepraChatText", {val: username+": "});
+        chatInput.val(username+": ");
     });
 })();
